a1238df550bac1bcd94013ab9f8ce93545772bf7
tayurus <tayurus@gmail.com>
2018-12-06 10:36:41 +0300

    chart

--- reactD3ChartCreator/precache-manifest.68e5036ae24cbdd2ca978b4f214cfac1.js
+++ reactD3ChartCreator/precache-manifest.68e5036ae24cbdd2ca978b4f214cfac1.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "6030dc52613fe0e5a12b",
-    "url": "/static/js/main.6030dc52.chunk.js"
+    "revision": "4d1e50fe0d9f6a0b2abb",
+    "url": "/static/js/main.4d1e50fe.chunk.js"
   },
   {
     "revision": "ac23114fe8a3a69f7000",
     "url": "/static/js/1.ac23114f.chunk.js"
   },
   {
-    "revision": "6030dc52613fe0e5a12b",
+    "revision": "4d1e50fe0d9f6a0b2abb",
     "url": "/static/css/main.98e4a17d.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.e2bb2d2d.chunk.css"
   },
   {
-    "revision": "e21e6d6a9275e4249fbffd2f00ef2a77",
+    "revision": "ce6d4267e9e79c8f76a547bee89ec84f",
     "url": "/index.html"
   }
 ];
---
+++ reactD3ChartCreator/reactD3ChartCreator/precache-manifest.68e5036ae24cbdd2ca978b4f214cfac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d1e50fe0d9f6a0b2abb",
+    "url": "/static/js/main.4d1e50fe.chunk.js"
+  },
+  {
+    "revision": "ac23114fe8a3a69f7000",
+    "url": "/static/js/1.ac23114f.chunk.js"
+  },
+  {
+    "revision": "4d1e50fe0d9f6a0b2abb",
+    "url": "/static/css/main.98e4a17d.chunk.css"
+  },
+  {
+    "revision": "ac23114fe8a3a69f7000",
+    "url": "/static/css/1.e2bb2d2d.chunk.css"
+  },
+  {
+    "revision": "ce6d4267e9e79c8f76a547bee89ec84f",
+    "url": "/index.html"
+  }
+];
