ca10272a0d32cfc850a9f310e65c0f395c2bc6d8
tayurus <tayurus@gmail.com>
2019-01-05 16:52:38 +0400

    fix js css3d

--- css3d/index.js
+++ css3d/index.js
@@ -1,22 +1,44 @@
 const container3d = document.querySelector(".container3d");
 let rotateAllowed = true;
-window.onwheel = function(e) {
+
+function rotate(direction) {
   if (rotateAllowed) {
     rotateAllowed = false;
     allowRotate(1500);
     const currentAngle = parseInt(
       container3d.style.transform.match(/[0-9]+/gi) || 0
     );
-    if (e.deltaY > 0) {
-      container3d.style.transform = "rotateX(" + (currentAngle + 90) + "deg)";
-    } else {
+
+    if (direction === "top") {
       container3d.style.transform = "rotateX(" + (currentAngle - 90) + "deg)";
+    } else {
+      container3d.style.transform = "rotateX(" + (currentAngle + 90) + "deg)";
     }
   }
-};
+}
 
 function allowRotate(time) {
   setTimeout(() => {
     rotateAllowed = true;
   }, time);
 }
+
+document.onkeydown = function(e) {
+  switch (e.which) {
+    case 38: // up
+      rotate("top");
+      break;
+
+    case 40: // down
+      rotate("bottom");
+      break;
+  }
+};
+
+window.onwheel = function(e) {
+  if (e.deltaY > 0) {
+    rotate("bottom");
+  } else {
+    rotate("top");
+  }
+};
