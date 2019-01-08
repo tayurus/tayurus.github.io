b284c68a170a6f385af18c320fc6d66d946d927c
tayurus <tayurus@gmail.com>
2019-01-08 21:08:20 +0400

    add react-tetris

---
+++ react-tetris/precache-manifest.85e1ebeae1dda03b0f8c686c0c2b5fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08d3bf7b117c26d20457",
+    "url": "/static/js/main.08d3bf7b.chunk.js"
+  },
+  {
+    "revision": "df56dfb9e9026d0e1672",
+    "url": "/static/js/1.df56dfb9.chunk.js"
+  },
+  {
+    "revision": "08d3bf7b117c26d20457",
+    "url": "/static/css/main.09b11770.chunk.css"
+  },
+  {
+    "revision": "df56dfb9e9026d0e1672",
+    "url": "/static/css/1.51001104.chunk.css"
+  },
+  {
+    "revision": "3a6c775f6edae519800e95d7f80338ab",
+    "url": "/index.html"
+  }
+];
