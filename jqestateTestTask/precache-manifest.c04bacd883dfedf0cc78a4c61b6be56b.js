ebbfcad2ae7b43839720201cd30883abc8028bb9
tayurus <tayurus@gmail.com>
2019-01-14 10:54:15 +0400

    jqestatetestTask

---
+++ jqestateTestTask/precache-manifest.c04bacd883dfedf0cc78a4c61b6be56b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e318fbcb46b23769a1619e62d9860f07",
+    "url": "/static/media/sotki.e318fbcb.svg"
+  },
+  {
+    "revision": "a2ab8f3bec6b7f20b9d1d15e372e4b75",
+    "url": "/static/media/house.a2ab8f3b.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c311c40a19afac5c2a3",
+    "url": "/static/js/main.3c311c40.chunk.js"
+  },
+  {
+    "revision": "a120253ce43472d7d377",
+    "url": "/static/js/1.a120253c.chunk.js"
+  },
+  {
+    "revision": "3c311c40a19afac5c2a3",
+    "url": "/static/css/main.c4d350ca.chunk.css"
+  },
+  {
+    "revision": "499ae9d06e510f6e92799215b1fa13fc",
+    "url": "/index.html"
+  }
+];
