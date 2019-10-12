dc573bc45282399aecd5cc4c80c0a54b941e50f5
tayurus <tayurus@gmail.com>
2019-10-12 20:02:04 +0300

    buro

---
+++ buroTestTask/precache-manifest.5720f62adb4f86074d6665891c2ae6f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3ca7607d16d0b093563c286c10928ab",
+    "url": "/static/media/logo.c3ca7607.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fb09a1729a984369f952",
+    "url": "/static/js/main.fb09a172.chunk.js"
+  },
+  {
+    "revision": "92c81d7ae9fb24c719f7",
+    "url": "/static/js/1.92c81d7a.chunk.js"
+  },
+  {
+    "revision": "fb09a1729a984369f952",
+    "url": "/static/css/main.522b9d87.chunk.css"
+  },
+  {
+    "revision": "92c81d7ae9fb24c719f7",
+    "url": "/static/css/1.38d1e168.chunk.css"
+  },
+  {
+    "revision": "39290d42431d1db7cfe98ed5e5e094d7",
+    "url": "/index.html"
+  }
+];
