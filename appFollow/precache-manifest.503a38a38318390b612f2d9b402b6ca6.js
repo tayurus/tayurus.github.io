a1ae4fd85d2b695be21bcf199ea7a2ac23b77083
tayurus <tayurus@gmail.com>
2020-07-02 16:49:18 +0300

    appFolllow

---
+++ appFollow/precache-manifest.503a38a38318390b612f2d9b402b6ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a928df0c77944c2fa42c3d2c57d30c42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "107f0e8163af4c77c4c0",
+    "url": "./static/css/main.3f6b5016.chunk.css"
+  },
+  {
+    "revision": "7c7840514e2e0d508ef0",
+    "url": "./static/js/2.24ee25eb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.24ee25eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107f0e8163af4c77c4c0",
+    "url": "./static/js/main.0c098ab0.chunk.js"
+  },
+  {
+    "revision": "bb84baf0fe2dca893936",
+    "url": "./static/js/runtime-main.c7efcd3f.js"
+  }
+]);
